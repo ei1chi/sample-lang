ae51ab0ad469b4c90659d6d1a05d2bdba93ff9ee
ei1chi <test@example.com>
2018-06-20 18:13:29 +0900

    prepare for Pratt Parsing

--- ast/ast.go
+++ ast/ast.go
@@ -1,9 +1,14 @@
 package ast
 
-import "github.com/ei1chi/sample-lang/token"
+import (
+	"strings"
+
+	"github.com/ei1chi/sample-lang/token"
+)
 
 type Node interface {
 	TokenLiteral() string
+	String() string
 }
 
 type Stmt interface {
@@ -20,6 +25,16 @@ type Program struct {
 	Stmts []Stmt
 }
 
+func (p *Program) String() string {
+	var out strings.Builder
+
+	for _, s := range p.Stmts {
+		out.WriteString(s.String())
+	}
+
+	return out.String()
+}
+
 func (p *Program) TokenLiteral() string {
 	if len(p.Stmts) > 0 {
 		return p.Stmts[0].TokenLiteral()
@@ -33,6 +48,20 @@ type LetStmt struct {
 	Value Expr
 }
 
+func (l *LetStmt) String() string {
+	var out strings.Builder
+
+	out.WriteString(l.TokenLiteral() + " ")
+	out.WriteString(l.Name.String())
+	out.WriteString(" = ")
+	if l.Value != nil {
+		out.WriteString(l.Value.String())
+	}
+	out.WriteString(";")
+
+	return out.String()
+}
+
 func (l *LetStmt) stmtNode() {}
 
 func (l *LetStmt) TokenLiteral() string {
@@ -44,15 +73,49 @@ type ReturnStmt struct {
 	ReturnValue Expr
 }
 
+func (r *ReturnStmt) String() string {
+	var out strings.Builder
+
+	out.WriteString(r.TokenLiteral() + " ")
+	if r.ReturnValue != nil {
+		out.WriteString(r.ReturnValue.String())
+	}
+	out.WriteString(";")
+
+	return out.String()
+}
+
 func (r *ReturnStmt) stmtNode() {}
 
 func (r *ReturnStmt) TokenLiteral() string {
 	return r.Token.Literal
 }
 
+type ExprStmt struct {
+	Token token.Token // 式の最初のトークン
+	Expr  Expr
+}
+
+func (e *ExprStmt) String() string {
+	if e.Expr != nil {
+		return e.Expr.String()
+	}
+	return ""
+}
+
+func (e *ExprStmt) stmtNode() {}
+
+func (e *ExprStmt) TokenLiteral() string {
+	return e.Token.Literal
+}
+
 type Ident struct {
 	Token token.Token // == token.IDENT
 	Value string
+}
+
+func (i *Ident) String() string {
+	return i.Value
 }
 
 func (i *Ident) exprNode() {}
